987f817e3df139e53a63958e52b1bfd99dea8930
technocrats <technocrats@kiet.edu>
2024-02-16 20:09:42 +0530

    Cors error

--- app.ts
+++ app.ts
@@ -21,7 +21,7 @@ app.use(cookieParser());
 // cors => cross origin resource sharing
 app.use(
   cors({
-    origin: ["https://www.springbee.com/"],
+    origin: ["https://www.springbee.tech/"],
     credentials: true,
   })
 );
