132e63fedd9a77fadc21a6fbc2029cb78a83f30c
technocrats <technocrats@kiet.edu>
2024-02-16 15:32:22 +0530

    afdsf

--- app.ts
+++ app.ts
@@ -21,7 +21,7 @@ app.use(cookieParser());
 // cors => cross origin resource sharing
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://www.springbee.tech/"],
     credentials: true,
   })
 );
