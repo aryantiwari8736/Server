bc6cecdc710e979e4ff5dd13702027956c7fca93
technocrats <technocrats@kiet.edu>
2024-02-16 16:08:28 +0530

    fsf

--- app.ts
+++ app.ts
@@ -20,16 +20,10 @@ app.use(cookieParser());
 
 // cors => cross origin resource sharing
 
-app.use(
-  cors()
-);
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); 
-  next();
-});
+app.use(cors({
+  origin: 'https://www.springbee.tech/' // Replace with your Next.js origin
+}));
+
 
 // api requests limit
 const limiter = rateLimit({
