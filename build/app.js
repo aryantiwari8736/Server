12575e4fce10b7ba34f4eab0964453c119970550
technocrats <technocrats@kiet.edu>
2024-02-20 13:02:57 +0530

    springbee live updated

--- build/app.js
+++ build/app.js
@@ -23,7 +23,7 @@ exports.app.use(express_1.default.json({ limit: "50mb" }));
 exports.app.use((0, cookie_parser_1.default)());
 // cors => cross origin resource sharing
 exports.app.use((0, cors_1.default)({
-    origin: ['https://www.springbee.tech', 'https://www.springbee.live'],
+    origin: 'https://www.springbee.live',
     credentials: true
 }));
 // api requests limit
