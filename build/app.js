f7d81eefd81c4d0f777fc7ab924682c1fec2ccec
technocrats <technocrats@kiet.edu>
2024-02-20 13:19:01 +0530

    cors error

--- build/app.js
+++ build/app.js
@@ -23,7 +23,7 @@ exports.app.use(express_1.default.json({ limit: "50mb" }));
 exports.app.use((0, cookie_parser_1.default)());
 // cors => cross origin resource sharing
 exports.app.use((0, cors_1.default)({
-    origin: 'https://www.springbee.live',
+    origin:[ 'https://springbee.live','https://www.springbee.tech'],
     credentials: true
 }));
 // api requests limit
